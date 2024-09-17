76bdf24b498dd1691729c030f2c7df61ff5e0ec9
Matthijs <matthijs07g@gmail.com>
2024-09-17 14:47:20 +0200

    added kantine controller and small changes

---
+++ Voedselverspilling.API/Controllers/KantineController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Voedselverspilling.Domain.IRepositories;
+using Voedselverspilling.Domain.Models;
+
+namespace Voedselverspilling.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KantineController : Controller
+    {
+        private readonly IKantineRepository _kantineRepository;
+
+        public KantineController(IKantineRepository kantineRepository)
+        {
+            _kantineRepository = kantineRepository;
+        }
+
+        //GET all
+        [HttpGet]
+        public async Task<IEnumerable<Kantine>> GetAllKantines()
+        {
+            return await _kantineRepository.GetAllAsync();
+        }
+
+        //GET one
+        [HttpGet("{id}")]
+        public async Task<Kantine> GetKantineById(int id)
+        {
+            return await _kantineRepository.GetByIdAsync(id);
+        }
+
+        //POST
+        [HttpPost]
+        public async Task AddKantine(Kantine kantine)
+        {
+            await _kantineRepository.AddAsync(kantine);
+        }
+
+        //PUT
+        [HttpPut("{id}")]
+        public async Task UpdateKantine(int id, Kantine kantine)
+        {
+            await _kantineRepository.UpdateAsync(kantine);
+        }
+
+        //DELETE
+        [HttpDelete("{id}")]
+        public async Task DeleteKantine(int id)
+        {
+            await _kantineRepository.DeleteAsync(id);
+        }
+    }
+}
--- Voedselverspilling.Domain/Models/Kantine.cs
+++ Voedselverspilling.Domain/Models/Kantine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Voedselverspilling.Domain.Models
 {
     public class Kantine
     {
+        [Key]
         public int Id { get; set; }
         public string? Stad {  get; set; }
         public string? Locatie { get; set; }
