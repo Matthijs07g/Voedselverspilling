e99bc4a5308bd90b2609ddab1cd3f1203d3b4003
Matthijs van Gastel <mmj.vangastel@student.avans.nl>
2024-10-24 21:39:31 +0000

    Deleted BordspellenV4

--- BordspellenV4/Models/TestModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Models;
-
-namespace BordspellenV4.Models
-{
-    public class TestModel
-    {
-        public IEnumerable<Game> Games { get; set; } = Enumerable.Empty<Game>();
-    }
-}
